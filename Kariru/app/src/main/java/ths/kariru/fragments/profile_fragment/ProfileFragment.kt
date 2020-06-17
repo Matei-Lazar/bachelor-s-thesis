061091b827247e779c4b8b49f0ad8ffdb83c624e
Matei Lazar <matei.lazar97@gmail.com>
2020-06-17 21:16:51 +0300

    added bottom navigation bar and fragments

---
+++ Kariru/app/src/main/java/ths/kariru/fragments/profile_fragment/ProfileFragment.kt
@@ -0,0 +1,18 @@
+package ths.kariru.fragments.profile_fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import ths.kariru.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
